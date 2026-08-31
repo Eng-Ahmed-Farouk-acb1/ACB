2036b19d6d0635e1ba8c141615a85bee5f7e6c51
Eng-Ahmed-Farouk-acb1 <acb1@adapt-eg.org>
2026-09-01 01:32:09 +0300

    Update CORS allowed origins in main.py

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app = fastapi.FastAPI()
 
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
-    allow_origins=["https://eng-ahmed-farouk-projects.github.io"],
+    allow_origins=["https://eng-ahmed-farouk-acb1.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
